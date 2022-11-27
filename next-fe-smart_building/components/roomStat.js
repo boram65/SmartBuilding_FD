30937d2d116df9608d6f2f1a04eedc245e67db1b
boram65 <5487032@gmail.com>
2022-11-27 22:16:28 +0900

    데이터 프롭스 전단 완료 {실시간 및 차트 남음}

--- next-fe-smart_building/components/roomStat.js
+++ next-fe-smart_building/components/roomStat.js
@@ -3,36 +3,32 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function roomStat(props) {
-  // props.data.map(e => {
-  //   console.log(e);
-  // });
-  // console.log(props.data);
+  //console.log(props);
 
-  return props.data.map((e, idx) => {
-    return (
-      <div
-        className="flex m-1 hover:bg-violet-400"
-        onClick={() => {
-          roomCHOI();
-        }}
-        key={idx}
-      >
-        <div className="flex-row border border-indigo-200">
-          <div className="flex justify-center text-xl mt-3 mx-8 text-gray-800">
-            <div className="border rounded-xl border-indigo-500 bg-indigo-300 w-16 flex justify-center">
-              방번호
-            </div>
+  return (
+    <div
+      className="flex m-1 hover:bg-violet-400"
+      onClick={() => {
+        props.detailData(props.Rnum);
+      }}
+    >
+      <div className="flex-row border border-indigo-200">
+        <div className="flex justify-center text-xl mt-3 mx-8 text-gray-800">
+          <div className="border rounded-xl border-indigo-500 bg-indigo-300 w-16 flex justify-center">
+            {props.data.room}
           </div>
-          <div className="relative pt-2">
-            <Image
-              src={roomLogo}
-              alt="stat"
-              className="w-20 h-20 mx-5 mb-3"
-            ></Image>
-            <div className="absolute top-0 right-4 text-3xl">🟢</div>
+        </div>
+        <div className="relative pt-2">
+          <Image
+            src={roomLogo}
+            alt="stat"
+            className="w-20 h-20 mx-5 mb-3"
+          ></Image>
+          <div className="absolute top-0 right-4 text-3xl">
+            {props.data.co2 > 600 ? "🔴" : "🟢"}
           </div>
         </div>
       </div>
-    );
-  });
+    </div>
+  );
 }
